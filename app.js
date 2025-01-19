cdec25ab0e63719bd2a85425b4a30012cc243759
nanohad <faisalganjadeveloper@gmail.com>
2025-01-19 14:20:42 +0000

    added files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from ECS!');
+  res.send('Hello from ECS 2!');
 });
 
 app.listen(port, () => {
